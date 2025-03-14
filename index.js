3bc37c84fe7bfecf1455324a9899329d3b097cc1
fedejael <federicojael.canto@gmail.com>
2025-03-13 21:09:26 -0300

    Añadiendo mas rutas, instalando TailwindCSS y creando Layout

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express()
 app.set("view engine", "pug")
 app.set("views", "./views")
 
+// Carpeta Pública
+app.use( express.static("public") )
 
 // Routing
 app.use("/auth", usuarioRoutes)
